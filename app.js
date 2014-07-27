faf60dffafd3f6659761a2f6b1425c3c51b0ef0c
Amanda Wong <wongamun@gmail.com>
2014-07-27 19:28:04 +0000

    Added my name

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ appHelper.runApp(function(app, db) {
 
 	console.log("Travel application is running!");
 
-	var myName = "YOUR_NAME_HERE";
+	var myName = "Amanda";
 	app.get("/", function (req, resp) {
 		resp.write("<html><body><h1>Welcome to "+myName+"'s first web app!</body></html>");
 		resp.end();
